ed38a9345cf95f7c0acab5c0bccbc5f372b8e2d9
Kabelo Moobi <giftmoobi@gmail.com>
2025-06-13 16:33:59 +0200

    chore: add script to install Apollo Server and related packages

---
+++ apollo.sh
@@ -0,0 +1,11 @@
+npm uninstall @apollo/server graphql \
+    @graphql-tools/schema @graphql-tools/merge @graphql-tools/load-files \
+    apollo-server-core apollo-server-express \
+    graphql-subscriptions @graphql-tools/subscriptions \
+    @graphql-tools/utils \
+    @graphql-tools/delegate && npm uninstall -D @types/graphql @types/graphql-subscriptions \
+    graphql-tag \
+    @graphql-codegen/cli \
+    @graphql-codegen/typescript \
+    @graphql-codegen/typescript-resolvers \
+    @graphql-codegen/introspection
